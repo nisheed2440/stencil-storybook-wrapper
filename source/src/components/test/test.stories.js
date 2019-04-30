9b2d34275957309cc342f3770b4289deaedf823c
Tom Chinery <tom.chinery@addtoevent.co.uk>
2019-04-30 17:51:23 +0100

    Remove: Test components accidently committed in

--- source/src/components/test/test.stories.js
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from '@storybook/html';
-
-storiesOf('Components/Test', module)
-  .add('Default', () => `<test></test>`)
-  .add(
-    'With Input',
-    () => `<test test="John"></test>`,
-  );
